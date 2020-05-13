2c692345102b977c849e38d3039075b69ffbedd4
Dmitry Danilov <daniloff200@gmail.com>
2020-05-13 20:29:48 +0300

    feat(tests): add new test, check all songs visible

--- favorite-songs-tdd-project/src/app/favorite-songs/favorite-songs.component.spec.ts
+++ favorite-songs-tdd-project/src/app/favorite-songs/favorite-songs.component.spec.ts
@@ -3,6 +3,12 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { FavoriteSongsComponent } from './favorite-songs.component';
 import { By } from '@angular/platform-browser';
 
+const favoriteSongsMock: any[] = [
+  { title: 'What goes around comes around', singer: 'Justin Timberlake' },
+  { title: 'Careless Whisper', singer: 'George Michael' },
+  { title: 'Highway To Hell', singer: 'AC/DC'},
+];
+
 describe('FavoriteSongsComponent', () => {
   let component: FavoriteSongsComponent;
   let fixture: ComponentFixture<FavoriteSongsComponent>;
@@ -24,5 +30,11 @@ describe('FavoriteSongsComponent', () => {
       const titleElements = fixture.debugElement.queryAll(By.css('h1'));
       expect(titleElements.length).toBe(1);
       expect(titleElements[0].nativeElement.innerHTML).toBe('My favorite songs');
+  });
+
+
+  it('show all the favorite songs', () => {
+    const songElements = fixture.debugElement.queryAll(By.css('.song'));
+    expect(songElements.length).toBe(favoriteSongsMock.length);
   });
 });
