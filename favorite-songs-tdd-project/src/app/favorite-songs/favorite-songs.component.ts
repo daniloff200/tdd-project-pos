28b97a9d5ae04b92f737472a81042d27991db223
Dmitry Danilov <daniloff200@gmail.com>
2020-05-14 15:14:00 +0300

    fix(favorite-songs): add data to the component

--- favorite-songs-tdd-project/src/app/favorite-songs/favorite-songs.component.ts
+++ favorite-songs-tdd-project/src/app/favorite-songs/favorite-songs.component.ts
@@ -9,6 +9,12 @@ export class FavoriteSongsComponent implements OnInit {
 
   constructor() { }
 
+  favoriteSongs: any[] = [
+    { title: 'What goes around comes around', singer: 'Justin Timberlake' },
+    { title: 'Careless Whisper', singer: 'George Michael' },
+    { title: 'Highway To Hell', singer: 'AC/DC'},
+  ];
+
   ngOnInit(): void {
   }
 
